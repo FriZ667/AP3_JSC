a6b395a79cebc96b00a0f891a04b08ad002a090f
FriZ <156026457+FriZ667@users.noreply.github.com>
2025-10-03 08:55:49 +0200

    test

---
+++ AP3_JSC/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace AP3_JSC
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}

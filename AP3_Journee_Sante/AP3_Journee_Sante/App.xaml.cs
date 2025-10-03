f3388118a02cfa468c121df06b9a07a3bebe9c82
FriZ <156026457+FriZ667@users.noreply.github.com>
2025-10-03 09:37:25 +0200

    Add files via upload

---
+++ AP3_Journee_Sante/AP3_Journee_Sante/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace AP3_Journee_Sante
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}

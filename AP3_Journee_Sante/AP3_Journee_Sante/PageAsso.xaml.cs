5af449a9237b9806ec6889d7a6373f18753c19eb
Arthur <arthurjoux8@gmail.com>
2025-10-03 10:44:40 +0200

    Pages principaux

---
+++ AP3_Journee_Sante/AP3_Journee_Sante/PageAsso.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AP3_Journee_Sante
+{
+    /// <summary>
+    /// Logique d'interaction pour PageAsso.xaml
+    /// </summary>
+    public partial class PageAsso : Page
+    {
+        public PageAsso()
+        {
+            InitializeComponent();
+        }
+    }
+}
